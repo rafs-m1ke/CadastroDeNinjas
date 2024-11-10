734e4241df02cc71a7ed73d53e7500b150dfae91
Rafael Silva <rafaelmike33@gmail.com>
2024-11-10 13:22:00 -0300

    feat: Inicia a implementação de DTO e Mapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,18 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
 }
